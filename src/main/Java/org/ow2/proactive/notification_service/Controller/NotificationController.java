c283d6dc0e7111747ba5743fb3d548b58f666f65
sophiesongge <sophiesongge@gmail.com>
2016-08-16 12:12:14 +0200

    change templates

--- src/main/Java/org/ow2/proactive/notification_service/Controller/NotificationController.java
+++ src/main/Java/org/ow2/proactive/notification_service/Controller/NotificationController.java
@@ -4,33 +4,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import org.ow2.proactive.notification_service.Model.Notification;
 import org.ow2.proactive.notification_service.Service.NotificationService;
 
-@Controller
+@RestController
+@RequestMapping(value = "/notification")
 public class NotificationController {
 
-    private final NotificationService notificationService;
 
-    @Autowired
-    public NotificationController(NotificationService notificationService){
-        this.notificationService = notificationService;
-    }
 
-    @RequestMapping(value = "", method = RequestMethod.POST)//what should the value be?
-    public ModelAndView Notification(@PathVariable("notification") Notification notification){
+    @RequestMapping(method = RequestMethod.POST)//what should the value be?
+    public ModelAndView Notification(@RequestBody Notification notification){
 
         ModelAndView model = new ModelAndView();
 
         model.setViewName("index");
 
-        model.addObject("date", notificationService.getDate(notification));
-        model.addObject("description", notificationService.getDescription(notification));
+        model.addObject("date", notification.getDate());
+        model.addObject("description", notification.getDescription());
 
         return model;
     }
